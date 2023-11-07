85f7b7f30e3ed8eaf1cdef9e34e61fd1bd3e79a6
JeongMin-D <j25ng.py@gmail.com>
2023-11-08 06:12:04 +0900

    d

--- LiDARcallback.py
+++ LiDARcallback.py
@@ -28,6 +28,19 @@ def get_modified_ranges_from_database():
     finally:
         connection.close()
 
+def generate_intensities_from_ranges(ranges):
+    min_intensity = 0.0
+    max_intensity = 234.0
+
+    # ranges의 최소값과 최대값을 구합니다.
+    min_range = 0.0
+    max_range = 8.0
+
+    # ranges에 비례하여 intensities를 생성합니다.
+    intensities = [(max_intensity - min_intensity) * (max_range - r) / (max_range - min_range) + min_intensity for r in ranges]
+
+    return intensities
+
 def laser_scan_callback(msg):
     # LaserScan 메시지 수신 시 호출되는 콜백 함수
     # msg.ranges를 변경하여 원하는 대로 데이터를 수정한다.
@@ -35,34 +48,39 @@ def laser_scan_callback(msg):
     # 예제: ranges 값 모두 2.0으로 변경
     modified_ranges = get_modified_ranges_from_database()
     modified_ranges = ast.literal_eval(modified_ranges)
+    # modified_ranges = [1.0] * len(msg.ranges)
 
-    # # 새로운 LaserScan 메시지 생성
-    # custom_laser = LaserScan(
-    #     header=msg.header,
-    #     angle_min=msg.angle_min,
-    #     angle_max=msg.angle_max,
-    #     angle_increment=msg.angle_increment,
-    #     time_increment=msg.time_increment,
-    #     scan_time=msg.scan_time,
-    #     range_min=msg.range_min,
-    #     range_max=msg.range_max,
-    #     ranges=modified_ranges,
-    #     intensities=msg.intensities
-    # )
+    # intensities 값 생성
+    modified_intensities = generate_intensities_from_ranges(modified_ranges)
+
 
     # 새로운 LaserScan 메시지 생성
     custom_laser = LaserScan(
         header=msg.header,
-        angle_min=0,
-        angle_max=359,
-        angle_increment=1,
+        angle_min=msg.angle_min,
+        angle_max=msg.angle_max,
+        angle_increment=msg.angle_increment,
         time_increment=msg.time_increment,
         scan_time=msg.scan_time,
         range_min=msg.range_min,
         range_max=msg.range_max,
         ranges=modified_ranges,
-        intensities=msg.intensities
+        intensities=modified_intensities
     )
+
+    # # 새로운 LaserScan 메시지 생성
+    # custom_laser = LaserScan(
+    #     header=msg.header,
+    #     angle_min=0,
+    #     angle_max=6.26573,
+    #     angle_increment=0.0174533,
+    #     time_increment=msg.time_increment,
+    #     scan_time=msg.scan_time,
+    #     range_min=msg.range_min,
+    #     range_max=msg.range_max,
+    #     ranges=modified_ranges,
+    #     intensities=modified_intensities
+    # )
 
     # 새로운 토픽으로 발행
     pub.publish(custom_laser)
